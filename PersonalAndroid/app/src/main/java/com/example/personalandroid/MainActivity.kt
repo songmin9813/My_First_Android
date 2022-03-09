104ea545f80fe9e7b6338250181174f52371d7e2
songmin9813 <mithan1@naver.com>
2022-03-09 13:56:53 +0900

    뷰 바인딩 학습

--- PersonalAndroid/app/src/main/java/com/example/personalandroid/MainActivity.kt
+++ PersonalAndroid/app/src/main/java/com/example/personalandroid/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.personalandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
-
+import com.example.personalandroid.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
     fun convertCurrency(view:View){
-        if(dollarText.text.isNotEmpty()){
-            val dollarValue=dollarText.text.toString().toFloat()
+        if(binding.dollarText.text.isNotEmpty()){
+            val dollarValue=binding.dollarText.text.toString().toFloat()
             val euroValue=dollarValue*0.85f
-            textView.text=euroValue.toString()
+            binding.textView.text=euroValue.toString()
         }
         else{
-            textView.text=getString(R.string.no_value_string)
+            binding.textView.text=getString(R.string.no_value_string)
         }
     }
 }
